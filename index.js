ba26ab00582a1c625948e5ae1d3d80c3dc61af5c
ruffkaw <shkliarvlad@gmail.com>
2025-04-04 13:05:48 +0300

    wwww

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const authBtns = document.getElementsByClassName("sig-but")
 const form = document.getElementById("authForm");
 const body = document.getElementById("body");
+
+const closeAuthModal = () => {
+    form.classList.remove("form-open");
+    body.classList.remove("form-opened");
+}
+const openAuthModal = () => {
+    form.classList.add("form-open");
+    body.classList.add("form-opened");
+}
 for (let i = 0; i < authBtns.length ; i++) {
     authBtns[i].addEventListener("click", () => {
-        form.classList.add("form-open");
-        body.classList.add("form-opened");
+        openAuthModal();
     })
 }
 
@@ -12,7 +20,19 @@ form.getElementsByClassName("form__inner")[0].addEventListener("click", (e) => {
     e.stopPropagation();
 })
 
-form.addEventListener("click", (e) => {
-    form.classList.remove("form-open");
-    body.classList.remove("form-opened");
+
+
+form.addEventListener("click", () => {
+    closeAuthModal();
+})
+
+form.getElementsByTagName("form")[0].addEventListener("submit", (e) => {
+    e.preventDefault();
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    if (!email || !password) {
+        return;
+    }
+    console.log({email, password});
+    closeAuthModal();
 })
